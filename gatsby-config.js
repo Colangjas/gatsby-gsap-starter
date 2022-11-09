5a6e2b333f7424fe1419afc8721bce3a657dd5e1
Colin Thompson <thompsoncolinm@gmail.com>
2022-11-09 14:07:06 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,13 @@ module.exports = {
     title: `Gatsby Gsap Starter`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-plugin-postcss", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  }
+]
 };
